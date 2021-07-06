c14f93236b5b6812680f354052a5dd0b95c43cf8
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2021-07-05 20:04:13 -0500

    commit tree 41d05bf6afa8af58a67d1005ceacae7cfce0cb1e

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+    
+
+    return sourceArray.map( x => x * -1)
+}
+
+function mapToNoChange(sourceArray) {
+    return sourceArray.map(x => x )
+}
+
+function mapToDouble(sourceArray) {
+    return sourceArray.map(x => x * 2)
+}
+
+function mapToSquare(sourceArray) {
+    return sourceArray.map( x => x**2)
+}
+
+
+function reduceToTotal(sourceArray, startingPoint = 0) {
+    let reducer = (accumulator, currentValue) => accumulator + currentValue
+
+    return sourceArray.reduce(reducer, startingPoint)
+}
+
+function reduceToAllTrue(sourceArray) {
+    let reducer = (accumulator, currentValue) => {
+        if (!!accumulator == true && !!currentValue == true) {
+            return true
+        }else{
+            return false
+        }
+    }
+    return sourceArray.reduce(reducer, true)
+}
+
+function reduceToAnyTrue(sourceArray) {
+ let reducer = (accumulator, currentValue) => {
+     if(!!accumulator == true || !!currentValue == true) {
+         return true
+     }else{
+         return false
+     }
+ }
+ return sourceArray.reduce(reducer, false, true)
+}
